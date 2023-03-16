9a7dee719f11350e1e61e91b073a2094f570a575
KYIMINHAN <kyiminhan@KYIMINHANs-MacBook-Air.local>
2023-03-16 20:15:34 +0630

    User List UI

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -10,61 +10,61 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h1>User List Here...</h1>
-                </div>
-                <div class="mt-4 -mb-3">
-                    <div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
-                        <div class="relative rounded-xl overflow-auto">
-                            <div class="shadow-sm overflow-hidden my-8">
-                                <table class="border-collapse border border-slate-400 table-auto w-full text-sm">
-                                    <thead>
-                                        <tr>
-                                            <th
-                                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                                                Song</th>
-                                            <th
-                                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                                                Artist</th>
-                                            <th
-                                                class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                                                Year</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody class="bg-white dark:bg-slate-800">
-                                        <tr>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                Malcolm Lockyer</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                1961</td>
-                                        </tr>
-                                        <tr>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                Witchy Woman</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                The Eagles</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                1972</td>
-                                        </tr>
-                                        <tr>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                Shining Star</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                Earth, Wind, and Fire</td>
-                                            <td
-                                                class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
-                                                1975</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                    <div class="mt-4 -mb-3">
+                        <div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
+                            <div id="content-wrapper" class="relative rounded-xl overflow-auto">
+                                <div class="shadow-sm overflow-hidden my-8">
+                                    <table class="border-collapse border border-slate-400 table-auto w-full text-sm">
+                                        <thead>
+                                            <tr>
+                                                <th
+                                                    class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                                    Song</th>
+                                                <th
+                                                    class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                                    Artist</th>
+                                                <th
+                                                    class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                                                    Year</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="bg-white dark:bg-slate-800">
+                                            <tr>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    Malcolm Lockyer</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    1961</td>
+                                            </tr>
+                                            <tr>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    Witchy Woman</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    The Eagles</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    1972</td>
+                                            </tr>
+                                            <tr>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    Shining Star</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    Earth, Wind, and Fire</td>
+                                                <td
+                                                    class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                                    1975</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
